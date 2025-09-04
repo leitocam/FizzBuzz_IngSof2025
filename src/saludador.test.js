5c9661b417d1bd7c4411ba07e18a828e7cc20973
leitocam <leonardo.camacho@ucb.edu.bo>
2025-09-03 20:47:05 -0400

    test saludador 2

--- src/saludador.test.js
+++ src/saludador.test.js
@@ -2,3 +2,8 @@ import saludador from "./saludador.js";
 test('Iteración 1: saluda con "Hola"', () => {
     expect(saludador()).toBe("Hola");
 });
+//iteracion 2: saluda con "Hola {nombre}" programa
+test('Iteración 2: saluda con "Hola {nombre}"', () => {
+    expect(saludador("María")).toBe("Hola María");
+    expect(saludador("Pedro")).toBe("Hola Pedro");
+});
