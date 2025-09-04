05ede0d71697e263d7843bf7f6a70ca2a412d6eb
leitocam <leonardo.camacho@ucb.edu.bo>
2025-09-03 20:55:21 -0400

    Funcion configurada con  saludo con hora

--- src/saludador.js
+++ src/saludador.js
@@ -1,10 +1,17 @@
 //saludador de acuerdo a la iteracion 
 //iteracion 1: saluda con "Hola" programa 
 //iteracion 2: saluda con "Hola {nombre}" si el nombre es valido
-export default function saludador(nombre) {
-    if (nombre) {
-        return `Hola ${nombre}`;
-    } else {
+//iteracion 3:Saludar de acuerdo a la hora del dia utilizando let fechaActual = new Date();
+//let horaActual = fechaActual.getHours();
+export default function saludador(nombre, hora) {
+    if (!nombre && !hora) {
         return "Hola";
-    }   
+    } else if (nombre && !hora) {
+        return `Hola ${nombre}`;
+    } else if (nombre && hora < 12) {
+        return `Buenos días ${nombre}`;
+    } else if (nombre && hora >= 12) {
+        return `Buenas tardes ${nombre}`;
+    }
+    return "Hola";
 }
