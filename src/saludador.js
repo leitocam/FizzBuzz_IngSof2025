f5f4b201861782fb74d9bc0eb944ec6cf8ee579d
leitocam <leonardo.camacho@ucb.edu.bo>
2025-09-03 20:49:30 -0400

    Modificación funcion saludador paso 2

--- src/saludador.js
+++ src/saludador.js
@@ -1,6 +1,10 @@
 //saludador de acuerdo a la iteracion 
 //iteracion 1: saluda con "Hola" programa 
-function saludador() {
-    return "Hola";
+//iteracion 2: saluda con "Hola {nombre}" si el nombre es valido
+export default function saludador(nombre) {
+    if (nombre) {
+        return `Hola ${nombre}`;
+    } else {
+        return "Hola";
+    }   
 }
-export default saludador;
