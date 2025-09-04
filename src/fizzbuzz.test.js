a4088eec9c0ea628a1dae61304bf245a6b8fa3a7
leitocam <leonardo.camacho@ucb.edu.bo>
2025-09-03 23:13:32 -0400

    Test multipos 3

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -2,3 +2,10 @@ import { fizzbuzz } from "./FizzBuzz"
 test("Devuelve '1' para el número 1", () => {
   expect(fizzbuzz(1)).toBe("1");
 });
+
+//Test multiplos de 3
+test("Devuelve 'Fizz' para múltiplos de 3", () => {
+    expect(fizzbuzz(3)).toBe("Fizz");
+    expect(fizzbuzz(6)).toBe("Fizz");
+  });
+  
