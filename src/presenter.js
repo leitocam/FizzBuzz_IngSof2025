55618314e9e027dbf04fd5029f32c4ad7adcea84
leitocam <leonardo.camacho@ucb.edu.bo>
2025-09-04 00:08:03 -0400

    refactorización

--- src/presenter.js
+++ src/presenter.js
@@ -1,39 +1,16 @@
-import sumar from "./sumador";
-import saludador from "./saludador";
+import { fizzbuzz } from "./FizzBuzz";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+// Elementos del FizzBuzz
+const numeroInput = document.querySelector("#numero");
+const fizzbuzzForm = document.querySelector("#fizzbuzz-form");
+const fizzbuzzDiv = document.querySelector("#fizzbuzz-div");
 
-// Elementos del saludador
-const nombre = document.querySelector("#nombre");
-const genero = document.querySelector("#genero");
-const edad = document.querySelector("#edad");
-const idioma = document.querySelector("#idioma");
-const saludarForm = document.querySelector("#saludar-form");
-const saludoDiv = document.querySelector("#saludo-div");
-
-form.addEventListener("submit", (event) => {
+fizzbuzzForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
-
-saludarForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let fechaActual = new Date();
-  let horaActual = fechaActual.getHours();
-  
-  const nombreValue = nombre.value || undefined;
-  const generoValue = genero.value || undefined;
-  const edadValue = edad.value ? Number.parseInt(edad.value) : undefined;
-  const idiomaValue = idioma.value || "ES";
-
-  const saludo = saludador(nombreValue, horaActual, generoValue, edadValue, idiomaValue);
-  saludoDiv.innerHTML = "<p>" + saludo + "</p>";
+  const num = Number.parseInt(numeroInput.value);
+  if (num && num > 0) {
+    const sequence = Array.from({length: num}, (_, i) => fizzbuzz(i + 1));
+    fizzbuzzDiv.innerHTML = "<p>" + sequence.join(", ") + "</p>";
+  }
 });
